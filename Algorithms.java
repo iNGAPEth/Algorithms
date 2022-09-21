051b7179832375457da14041e08b054b320de87d
iNGAPEth <69355827+iNGAPEth@users.noreply.github.com>
2022-09-22 05:13:38 +0900

    Update Algorithms.java

--- Algorithms.java
+++ Algorithms.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 public class Algorithms {
 
     private static Scanner in = new Scanner(System.in);
-
     public static void main(String[] args) {
 
         int number = in.nextInt();
 
         if (number > 7) {
+            
             System.out.println("Привет");
-        }
-        else if (number <= 7) {
+            
+        } else if (number <= 7) {
+            
             System.out.println("Досвидания");
         }
     }
